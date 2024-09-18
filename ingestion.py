87d5749af65b503ee4d9e4ac18bd64f4b4ea8c82
Linoy Elimeleh <lino1998y@gmail.com>
2024-09-18 10:33:53 +0300

    add course details

--- ingestion.py
+++ ingestion.py
@@ -1,9 +1,13 @@
+import certifi
+import os
+os.environ['SSL_CERT_FILE'] = certifi.where()
+
 from dotenv import load_dotenv
 
 load_dotenv()
-
+from langchain_community.document_loaders import UnstructuredPowerPointLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import ReadTheDocsLoader
+# from langchain_community.document_loaders import ReadTheDocsLoader
 from langchain_openai import OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
 
@@ -13,21 +17,26 @@ embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
 # Trnslate the data into pinecone
 # todo update it to read from goggle drive
 def ingest_docs():
-    loader = ReadTheDocsLoader("langchain-docs/api.python.langchain.com/en/latest")
+    folder_path = "intro-to-cs-public/lectures"
+    documents = []
 
-    raw_documents = loader.load()
-    print(f"loaded {len(raw_documents)} documents")
+    # Iterate through all .pptx files in the folder
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".pptx"):
+            file_path = os.path.join(folder_path, filename)
+            loader = UnstructuredPowerPointLoader(file_path)
+            raw_documents = loader.load()
+            print(f"Loaded {len(raw_documents)} documents from {filename}")
+            documents.extend(raw_documents)
+
+    print(f"Total loaded documents: {len(documents)}")
 
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=600, chunk_overlap=50)
-    documents = text_splitter.split_documents(raw_documents)
-    for doc in documents:
-        new_url = doc.metadata["source"]
-        new_url = new_url.replace("langchain-docs", "https:/")
-        doc.metadata.update({"source": new_url})
+    split_documents = text_splitter.split_documents(documents)
 
-    print(f"Going to add {len(documents)} to Pinecone")
+    print(f"Going to add {len(split_documents)} to Pinecone")
     PineconeVectorStore.from_documents(
-        documents, embeddings, index_name="cs-intro-chatbot"
+        split_documents, embeddings, index_name="cs-intro-chatbot"
     )
     print("****Loading to vectorstore done ***")
 
