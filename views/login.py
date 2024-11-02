1145f77c245573e495acbd2d3f0725a05799db13
Linoy Elimeleh <lino1998y@gmail.com>
2024-11-02 20:11:17 +0200

    fix firebase logs!

--- views/login.py
+++ views/login.py
@@ -3,10 +3,12 @@ import os
 import streamlit as st
 from views.header import render_header
 import logging
-from firebase_admin import auth, credentials, initialize_app, get_app
+from firebase_admin import auth, credentials, initialize_app, get_app, firestore
+import datetime
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
 
 try:
     # Check if the default app is already initialized
@@ -15,19 +17,30 @@ except ValueError:
     # If not, initialize it
     with open(os.getenv("FIREBASE_CONFIG")) as f:
         firebase_config = json.load(f)
-    firebase_config["private_key"] = os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n")
+    firebase_config["private_key"] = os.getenv(
+        "FIREBASE_PRIVATE_KEY").replace("\\n", "\n")
     firebase_config["client_email"] = os.getenv("FIREBASE_CLIENT_EMAIL")
     firebase_config["client_id"] = os.getenv("FIREBASE_CLIENT_ID")
     firebase_config["private_key_id"] = os.getenv("FIREBASE_PRIVATE_KEY_ID")
-    
+
     cred = credentials.Certificate(firebase_config)
     logger.info(f"Using credentials: {cred}")
     initialize_app(cred)
+
+db = firestore.client()
 
 
 def is_user_logged_in():
     """Check if the user is logged in and verify token if needed."""
     return st.session_state.get("logged_in", False) and st.session_state.get("user_email") is not None
+
+
+def log_sign_in_event(user_id, email):
+    db.collection('sign_in_logs').add({
+        'user_id': user_id,
+        'email': email,
+        'timestamp': datetime.datetime.utcnow()
+    })
 
 
 def authenticate_user(email):
@@ -37,6 +50,11 @@ def authenticate_user(email):
         st.session_state.user_email = email
         st.session_state.logged_in = True
         logger.info(f"User {email} authenticated successfully.")
+        log_sign_in_event(user.uid, email)
+        # analytics.log_event(
+        #     "user_sign_in",
+        #     {"email": email}
+        # )
 
     except auth.UserNotFoundError:
         st.error("Email doesn't exist.")
@@ -70,17 +88,17 @@ def login_interface():
         authenticate_user(email)
 
         if is_user_logged_in():
-            st.rerun() 
+            st.rerun()
 
     st.markdown('</div>', unsafe_allow_html=True)
 
-    st.markdown("<hr>", unsafe_allow_html=True)  
+    st.markdown("<hr>", unsafe_allow_html=True)
     st.markdown("""
         <div style='text-align: center; margin-top: 20px;'>
             <p style='font-size: 14px; color: gray;'>Do you have problems logging in? <a href='mailto:lino1998y@gmail.com'>Contact us</a></p>
         </div>
     """, unsafe_allow_html=True)
-    
+
     # Debugging: Uncomment to see session state
     # st.write("Debug: Session state", st.session_state)
 
